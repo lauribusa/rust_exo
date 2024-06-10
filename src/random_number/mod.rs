463eba0e49254edef57bf8a40ab8e53cfc96cb33
LBU <lauri.busa@gmail.com>
2024-06-10 16:28:05 +0200

    refactor into modules

---
+++ src/random_number/mod.rs
@@ -0,0 +1,31 @@
+	use std::time::{SystemTime, UNIX_EPOCH};
+
+pub fn roll_dice(){
+    let roll = rand();
+    let result = random_roll(roll);
+    println!("Battle results: {result} ({})", roll);
+}
+
+fn random_roll(num: u32) -> String{
+    if num > 17{
+        "Decisive Victory".to_string()
+    } else if num > 10{
+        "Close Victory".to_string()
+    } else if num > 2 {
+        "Pyrrhic Victory".to_string()
+    } else {
+        "Crushing Defeat".to_string()
+    }
+}
+	
+fn rand() -> u32 {
+	
+    let since_epoch = SystemTime::now().duration_since(UNIX_EPOCH)
+	
+        .expect("Le temps a reculé");
+	
+    let nanos = since_epoch.as_nanos();
+	
+    (nanos % 21) as u32
+	
+}
